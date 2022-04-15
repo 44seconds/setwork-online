58ebb6e5d563b89c9d6269f6c947641adfc64d34
Alan James <directedby44@gmail.com>
2022-04-15 02:09:16 +0100

    commit to 'Main'

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 function Header() {
   return (
     <header>
-      <div className="relative flex items-center h-10 cursor-pointer">
+      <div
+        className="relative flex items-center h-10 cursor-pointer 
+      my-auto"
+      >
         <Image
           src="https://setwork.ai/images/Setwork4-logo-new-tiny.png"
           layout="fill"
