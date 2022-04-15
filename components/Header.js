b55305f6ecebd15c04840c9ce54d67076759272a
Alan James <directedby44@gmail.com>
2022-04-15 01:59:12 +0100

    commit on 'Main'

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      <div className="relative flex items-center h-10 cursor-pointer">
+        <Image
+          src="https://setwork.ai/images/Setwork4-logo-new-tiny.png"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      <div></div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    (module.exports = {
+      content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+      // ...
+    }),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
